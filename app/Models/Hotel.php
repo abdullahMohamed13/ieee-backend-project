c5e67c6c3d5c854b804b75453866c41a47310643
rawn187 <rawanakh2005@gmail.com>
2026-06-27 23:12:02 +0300

    Hotel model and migration done

--- app/Models/Hotel.php
+++ app/Models/Hotel.php
@@ -6,6 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Hotel extends Model
 {
+    protected $fillable = [
+        'name',
+        'address',
+        'description',
+        'image',
+        'rating'
+    ];
+
     public function rooms()
     {
         return $this->hasMany(Room::class);
